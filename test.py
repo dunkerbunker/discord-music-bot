8556b1e117bddd930417d7a28143b8f08560e5e3
Yoosuf Sayyid <cena26657@gmail.com>
2022-04-23 14:38:50 +0500

    l

---
+++ test.py
@@ -0,0 +1,10 @@
+# importing the discord package
+import discord
+
+#client / bot
+client = discord.Client()
+
+#stuff
+
+#run the bot
+client.run('NzA0MzkzOTM2OTk0NTY2MTg0.Xqcf-Q.O2MrhJ6dzXYi5Y7nnt-o9uVE-BU')
